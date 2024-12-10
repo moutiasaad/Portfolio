b8f06c6911cc8fd7c384069b5a6e0e2f23243cd1
moutiasaad <moutiasaad481@gmail.com>
2024-12-10 12:59:13 +0100

    update cv

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,7 @@ import { Container } from './styles'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { NavHashLink, HashLink } from 'react-router-hash-link'
 import { useState } from 'react'
-import Resume from '../../assets/CV_Moutia_Bensaad3_eng.pdf'
+import Resume from '../../assets/CV_Moutia_Bensaad5_eng.pdf'
 export function Header() {
   const [isActive, setActive] = useState(false)
   function toggleTheme() {
