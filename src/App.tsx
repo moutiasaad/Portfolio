7277d654f329207da65ec51c1bb6d456d2956e62
moutiasaad <moutiasaad481@gmail.com>
2024-12-11 09:45:15 +0100

    add vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Footer } from './components/Footer/Footer'
 import { Header } from './components/Header/Header'
 import { Main } from './components/Main/Main'
 import { GlobalStyle } from './styles/global'
+import { Analytics } from "@vercel/analytics/react"
 // import 'react-toastify/dist/ReactToastify.css'
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Header></Header>
       <Main></Main>
       <Footer></Footer>
+      <Analytics />
     </>
   )
 }
